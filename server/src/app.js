2c3b0a1b09698c64210f1bffe19b0414624ab93e
aryakarthi <aryaperumal.m@gmail.com>
2025-01-25 15:30:45 +0530

    updated

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
-import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import authRoutes from "./routes/authRoute.js";
 import userRoutes from "./routes/userRoute.js";
+import connectMongoDB from "./config/mongo/dbConfig.js";
 
 dotenv.config();
-connectDB();
+connectMongoDB();
 
 const PORT = 5000;
 
@@ -13,7 +15,9 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
+app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
